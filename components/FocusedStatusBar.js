ce5060ea5d656fb44f17e3fece47fe6b315ca105
fatih <yvzfth@yandex.com>
2022-04-09 01:11:52 +0300

    finished home screen

---
+++ components/FocusedStatusBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StatusBar, StyleSheet, Text, View } from 'react-native';
+
+import { useIsFocused } from '@react-navigation/core';
+
+const FocusedStatusBar = (props) => {
+  const isFocused = useIsFocused();
+
+  return isFocused ? <StatusBar animated={true} {...props} /> : null;
+};
+
+export default FocusedStatusBar;
+
+const styles = StyleSheet.create({});
