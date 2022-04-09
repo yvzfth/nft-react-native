e24497b02e76a0eca77f96e3a93f9b01e4283d63
fatih <yvzfth@yandex.com>
2022-04-09 15:30:20 +0300

    build finished

--- components/index.js
+++ components/index.js
@@ -2,4 +2,16 @@ import HomeHeader from './HomeHeader';
 import FocusedStatusBar from './FocusedStatusBar';
 import NFTCard from './NFTCard';
 import { CircleButton, RectButton } from './Button';
-export { HomeHeader, FocusedStatusBar, NFTCard, CircleButton, RectButton };
+import { SubInfo } from './SubInfo';
+import DetailsDesc from './DetailsDesc';
+import DetailsBid from './DetailsBid';
+export {
+  HomeHeader,
+  FocusedStatusBar,
+  NFTCard,
+  CircleButton,
+  RectButton,
+  SubInfo,
+  DetailsDesc,
+  DetailsBid,
+};
